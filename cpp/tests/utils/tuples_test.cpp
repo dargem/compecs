623fdb3f54eee89d16eaaa55f9cecdb7e44dc6cf
dargem <genericz145@gmail.com>
2026-05-28 23:04:58 +1000

    tuple filter and convert tests

--- cpp/tests/utils/tuples_test.cpp
+++ cpp/tests/utils/tuples_test.cpp
@@ -5,6 +5,7 @@
 #include <type_traits>
 
 #include "gtest/gtest.h"
+#include "utils/packs.hpp"
 
 namespace compecs {
 
@@ -13,13 +14,21 @@ namespace {
 template <typename T>
 struct IntConvertible : std::bool_constant<std::is_convertible_v<T, int>> {};
 
+template <typename T>
+struct IsFloat : std::bool_constant<std::same_as<T, float>> {};
 }  // namespace
 
 TEST(TupleUtilities, TupleFilter) {
-    std::tuple<int, double, std::string, long> tup{};
+    std::tuple<int, double, std::string, long, float> tup{};
     TupleFilter<IntConvertible, decltype(tup)>::Result r;
 
-    ASSERT_TRUE((std::same_as<decltype(r), std::tuple<int, double, long>>));
+    ASSERT_TRUE((std::same_as<decltype(r), std::tuple<int, double, long, float>>));
+    ASSERT_TRUE((std::same_as<TupleFilter<IsFloat, decltype(tup)>::Result, std::tuple<float>>));
+}
+
+TEST(TupleUtilities, TupleConvert) {
+    TupleConvert<std::tuple<int, long, float>> t{};
+    ASSERT_TRUE((std::same_as<TypePack<int, long, float>, decltype(t)::Pack>));
 }
 
 }  // namespace compecs
