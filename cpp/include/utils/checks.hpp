8da937992027cc947e8a92d33a7c70897e4146d4
dargem <genericz145@gmail.com>
2026-05-29 14:03:56 +1000

    better lambda trait check

--- cpp/include/utils/checks.hpp
+++ cpp/include/utils/checks.hpp
@@ -10,11 +10,17 @@ namespace compecs {
 // aka it is low level const, but not high level const
 // is_const_v only checks high level const
 template <typename T>
-concept IsRead = std::is_const_v<std::remove_reference_t<T>> ||
-                 (!std::is_reference_v<T> && !std::is_rvalue_reference_v<T>);
+concept CheckRead = std::is_const_v<std::remove_reference_t<T>> ||
+                    (!std::is_reference_v<T> && !std::is_rvalue_reference_v<T>);
+
+template <typename T>
+struct IsRead : std::bool_constant<CheckRead<T>> {};
 
 // A non const reference is a write
 template <typename T>
-concept IsWrite = std::is_lvalue_reference_v<T> && !std::is_const_v<std::remove_reference_t<T>>;
+concept CheckWrite = std::is_lvalue_reference_v<T> && !std::is_const_v<std::remove_reference_t<T>>;
+
+template <typename T>
+struct IsWrite : std::bool_constant<CheckWrite<T>> {};
 
 }  // namespace compecs
--- cpp/tests/utils/lambda_traits_test.cpp
+++ cpp/tests/utils/lambda_traits_test.cpp
@@ -1,22 +1,29 @@
 #include "utils/lambda_traits.hpp"
 
+#include <string>
+
 #include "gtest/gtest.h"
 
 namespace compecs {
 
 namespace {
 using L = decltype([](int a, double b, float& c) { return 5.0f; });
-using Info = FunctionInfo<L>;
+using L2 = decltype([](double a, int b, std::string c) {});
+using LInfo = FunctionInfo<L>;
+using L2Info = FunctionInfo<L2>;
 }  // namespace
 
 TEST(LambdaTraits, ReturnType) {
     // Should deduce float
-    ASSERT_TRUE((std::same_as<float, Info::ReturnType>));
+    ASSERT_TRUE((std::same_as<float, LInfo::ReturnType>));
+    ASSERT_TRUE((std::same_as<void, L2Info::ReturnType>));
 }
 
-TEST(LambdaTraits, ArgPack) {
-    ASSERT_TRUE((std::same_as<TypePack<int, double, float&>, Info::ArgPack>));
-    ASSERT_TRUE((std::same_as<TypePack<int, double, float>, Info::DecayedArgPack>));
+TEST(LambdaTraits, ArgPacks) {
+    ASSERT_TRUE((std::same_as<TypePack<int, double, float&>, LInfo::ArgPack>));
+    ASSERT_TRUE((std::same_as<TypePack<int, double, float>, LInfo::DecayedArgPack>));
+
+    ASSERT_TRUE((std::same_as<TypePack<double, int, std::string>, L2Info::ArgPack>));
 }
 
 }  // namespace compecs
