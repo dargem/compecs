aec7b2be5e7728a0fc29506c9f21c26a79775e32
dargem <genericz145@gmail.com>
2026-05-29 14:11:11 +1000

    component def

---
+++ cpp/include/components/component_def.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <compare>
+#include <concepts>
+#include <type_traits>
+
+namespace compecs {
+
+// To be a component it must inherit a ComponentTag. Should have 0 overhead due to empty base
+// optimisation and it not relying on polymorphism at runtime.
+struct ComponentTag {
+    auto operator<=>(const ComponentTag&) const = default;
+};
+
+// A simple utility for creating basic components, e.g. To have a archetype hold 2 doubles for
+// something like x and y pos they cannot both be doubles since an archetype can't have multiple of
+// the same type. A type alias does not solve this since its just an alias not a different type. So
+// this allows creating a simple component like a X component that works like a given type (double)
+template <typename Tag, typename T>
+class BasicComponent : public ComponentTag {
+   public:
+    BasicComponent(T val) : val{val} {}
+    BasicComponent() : val{T{}} {}
+    operator T() const { return val; }
+    operator T&() { return val; }
+    operator const T&() const { return val; }
+    // Returns a comparison category object
+    auto operator<=>(const BasicComponent&) const = default;
+    bool operator==(const BasicComponent&) const = default;
+
+    friend auto operator<=>(const BasicComponent& lhs, const T& rhs)
+        requires std::three_way_comparable<T>
+    {
+        return lhs.val <=> rhs;
+    }
+
+    friend auto operator<=>(const T& lhs, const BasicComponent& rhs)
+        requires std::three_way_comparable<T>
+    {
+        return lhs <=> rhs.val;
+    }
+
+    friend bool operator==(const BasicComponent& lhs, const T& rhs)
+        requires std::equality_comparable<T>
+    {
+        return lhs.val == rhs;
+    }
+
+    friend bool operator==(const T& lhs, const BasicComponent& rhs)
+        requires std::equality_comparable<T>
+    {
+        return lhs == rhs.val;
+    }
+
+   private:
+    T val;
+};
+
+template <typename T>
+concept Component = std::derived_from<std::remove_cvref_t<T>, ComponentTag>;
+
+template <typename T>
+struct IsComponent : std::bool_constant<Component<T>> {};
+
+}  // namespace compecs
--- cpp/tests/utils/checks_test.cpp
+++ cpp/tests/utils/checks_test.cpp
@@ -8,18 +8,18 @@ namespace compecs {
 
 TEST(ECSConceptTests, IsRead) {
     // checks IsRead properly filters
-    EXPECT_TRUE((IsRead<int>)) << "Copy can't modify OG data so should be read";
-    EXPECT_TRUE((IsRead<const int>)) << "Const is superfluous but should also be a read";
-    EXPECT_FALSE((IsRead<int&>)) << "Non const reference is obv not just a read";
-    EXPECT_TRUE((IsRead<const int&>)) << "you wouldn't use const cast would you";
+    EXPECT_TRUE((CheckRead<int>)) << "Copy can't modify OG data so should be read";
+    EXPECT_TRUE((CheckRead<const int>)) << "Const is superfluous but should also be a read";
+    EXPECT_FALSE((CheckRead<int&>)) << "Non const reference is obv not just a read";
+    EXPECT_TRUE((CheckRead<const int&>)) << "you wouldn't use const cast would you";
 }
 
 TEST(ECSConceptTests, IsWrite) {
     // checks IsRead properly filters
-    EXPECT_FALSE((IsWrite<int>)) << "Copy can't modify OG data so not a write";
-    EXPECT_FALSE((IsWrite<const int>)) << "Const is superfluous, is also be not a write";
-    EXPECT_TRUE((IsWrite<int&>)) << "Non const reference is a write";
-    EXPECT_FALSE((IsWrite<const int&>)) << "you wouldn't use const cast would you";
+    EXPECT_FALSE((CheckWrite<int>)) << "Copy can't modify OG data so not a write";
+    EXPECT_FALSE((CheckWrite<const int>)) << "Const is superfluous, is also be not a write";
+    EXPECT_TRUE((CheckWrite<int&>)) << "Non const reference is a write";
+    EXPECT_FALSE((CheckWrite<const int&>)) << "you wouldn't use const cast would you";
 }
 
 }  // namespace compecs
